0cba4b34782d2b9ecf058727cf7636e89303b251
Nazar <nazar@settle.co>
2026-01-20 17:46:25 +0200

    Fix eslint config and static metadata routes

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,19 +1,10 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+import nextCoreWebVitalsConfig from 'eslint-config-next/core-web-vitals';
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals"),
+  ...nextCoreWebVitalsConfig,
   {
     rules: {
-      "@next/next/no-img-element": "off",
+      '@next/next/no-img-element': 'off',
     },
   },
 ];
